9627b145b0f908d412f47848f3585730e239c80e
Breno Pereira <brenomartinsxd@gmail.com>
2020-01-26 10:17:18 -0800

    Added: Changes into database connect and docker-compose container modify

--- products/config/database.js
+++ products/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   database: process.env.POSTGRES_DB || "postgres",
   username: process.env.POSTGRES_USER || "postgres",
   password: process.env.POSTGRES_PASSWORD || "postgres",
-  host: process.env.POSTGRES_HOST || "192.168.16.3",
+  host: process.env.POSTGRES_HOST || "localhost",
   dialect: "postgres",
   pool: {
     max: 5,
